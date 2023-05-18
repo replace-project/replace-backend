684ef3ea9995a4b11e2468688101c2974979a06c
tutti <80812697+JongMinCh0i@users.noreply.github.com>
2023-05-19 00:13:26 +0900

    Add files via upload

---
+++ re-place/src/main/java/com/replace/re/place/RePlaceApplication.java
@@ -0,0 +1,13 @@
+package com.replace.re.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RePlaceApplication.class, args);
+	}
+
+}
---
+++ re-place/src/test/java/com/replace/re/place/RePlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replace.re.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
