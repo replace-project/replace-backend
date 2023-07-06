1413482dfcac95cfbc00934ef7d4a2e9edbf0a76
함궈 <heoja0123@naver.com>
2023-07-06 14:56:52 +0900

    feat: 서비스단 리뷰 삭제 기능 구현 ( #9 )

--- re-place/src/main/java/com/replace/re/place/review/service/FacadeReviewService.java
+++ re-place/src/main/java/com/replace/re/place/review/service/FacadeReviewService.java
@@ -1,7 +1,9 @@
 package com.replace.re.place.review.service;
 
+import com.replace.re.place.category.dto.CategoryDto;
 import com.replace.re.place.category.service.CategoryService;
 import com.replace.re.place.image.service.ImageService;
+import com.replace.re.place.review.dto.GetDetailedReviewDto;
 import com.replace.re.place.review.dto.PostReviewDto;
 import com.replace.re.place.review.dto.ReviewDto;
 import com.replace.re.place.store.service.StoreSercvice;
@@ -24,23 +26,63 @@ public class FacadeReviewService {
 
 
     @Transactional
-    public void facade(PostReviewDto postReviewDto) throws IOException {
+    public void createReview(PostReviewDto postReviewDto) throws IOException {
         ReviewDto reviewDto = postReviewDto.getReviewDto();
-        // 리뷰 생성 로직 추가
-        Long reviewId = reviewService.insertReview(reviewDto);
 
+        // 리뷰 생성
+        Long reviewId = reviewService.insertReview(reviewDto);
 
         //이미지 저장
         List<MultipartFile> multipartFiles = postReviewDto.getMultipartFiles();
         imageService.insertReviewImage(reviewId, multipartFiles);
 
-
         //카테고리 저장
         List<String> categoryNames = postReviewDto.getCategoryNames();
         categoryService.insertReviewCategory(reviewId, categoryNames);
 
+        // 공간 저장
+        // ...
+    }
 
-        //공간 저장
 
+    @Transactional
+    public void deleteReview(Long reviewId){
+
+        // 리뷰 삭제
+        reviewService.deleteReview(reviewId);
+
+        // 이미지 삭제
+        imageService.deleteReviewImage(reviewId);
+
+        // 카테고리 삭제
+        categoryService.deleteReviewCategory(reviewId);
+
+        //공간 삭제
+        // ...
+    }
+
+
+
+    public GetDetailedReviewDto getReview(Long reviewId){
+
+        // 리뷰 조회
+        ReviewDto reviewDto = reviewService.getReview(reviewId);
+
+        // 이미지 조회
+        List<String> reviewImages = imageService.getReviewImages(reviewId);
+
+        // 카테고리 조회
+        List<CategoryDto> categoryDtos = categoryService.getReviewCategories(reviewId);
+
+        //공간 조회
+        // ...
+
+        GetDetailedReviewDto getDetailedReviewDto = GetDetailedReviewDto.builder()
+                .reviewDto(reviewDto)
+                .imageUrls(reviewImages)
+                .categoryDtos(categoryDtos).build();
+
+
+        return getDetailedReviewDto;
     }
 }
